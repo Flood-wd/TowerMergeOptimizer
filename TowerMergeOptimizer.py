01a372b79d49ad6733153fbe27c4c4a25d616706
Flood-wd <ohmizu.nojilab1026@gmail.com>
2025-09-04 19:23:19 +0900

    small revision

--- TowerMergeOptimizer.py
+++ TowerMergeOptimizer.py
@@ -13,7 +13,7 @@ def load_data():
     df_volt = pd.read_csv("https://raw.githubusercontent.com/Flood-wd/TowerMergeOptimizer/main/Data_.xlsx%20-%20Volt.csv")
     df_archmage = pd.read_csv("https://raw.githubusercontent.com/Flood-wd/TowerMergeOptimizer/main/Data_.xlsx%20-%20ArchMage.csv")
     df_flak = pd.read_csv("https://raw.githubusercontent.com/Flood-wd/TowerMergeOptimizer/main/Data_.xlsx%20-%20Flak.csv")
-    df_mage_lightning = pd.read_csv("https://raw.githubusercontent.com/Flood-wd/TowerMergeOptimizer/main/Data_.xlsx%20-%20Mage_lightnいいing.csv")
+    df_mage_lightning = pd.read_csv("https://raw.githubusercontent.com/Flood-wd/TowerMergeOptimizer/main/Data_.xlsx%20-%20Mage_lightning.csv")
     return df_cosmic_oculus, df_crystal_pylon, df_volt, df_archmage, df_flak, df_mage_lightning
 
 def optimize_merge(tower_type, initial_level, target_level, max_num, material_type,
